b6912076cf536ed37b1a9b94700d0d50efb56142
polawich <toniceee45@gmail.com>
2023-05-15 17:08:45 +0700

    add file

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const app = express();
 const morgan = require('morgan');
 const debug = require('debug')('app');
 const port = 3000;
+const path = require('path');
 
 app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname,"/public/")));
 
 app.get("/", (req,res) => {
     res.send("hello polawich123");
